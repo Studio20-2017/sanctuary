5f75f022af69c987338f2a2fee3a0e081fb91674
set <setarehbaig@gmail.com>
2017-04-25 23:06:48 -0400

    palestine map

---
+++ scripts/nerdeen.js
@@ -0,0 +1,49 @@
+window.mapIsChanging = false;
+
+function changeMap(newImage) {
+  if (!mapIsChanging) {
+    console.log("changing #map to "+newImage);
+    window.mapIsChanging = true;
+    $('#map').css("background-image", "url('"+newImage+"')");
+    setTimeout(function() {
+      window.mapIsChanging=false;
+    }, 1010);
+  }
+}
+
+$('#map-trigger-1').scrollie({
+    direction : 'up',
+    scrollOffset : 0,
+    scrollRatio : 2,
+    scrollingInView : function(){
+      changeMap('https://studio20-2017.github.io/sanctuary/images/westbankmap.jpg');
+    }
+  });
+
+ $('#map-trigger-2').scrollie({
+    direction : 'both',
+    scrollOffset : 0,
+    scrollRatio : 2,
+    scrollingInView : function(){
+      changeMap('https://studio20-2017.github.io/sanctuary/images/westbankmap2.jpg');
+    }
+  });
+
+
+ $('#map-trigger-3').scrollie({
+    direction : 'both',
+    scrollOffset : 0,
+    scrollRatio : 2,
+    scrollingInView : function(){
+      changeMap('https://studio20-2017.github.io/sanctuary/images/westbankmap3.jpg');
+    }
+  });
+
+ $('#map-trigger-4').scrollie({
+    direction : 'both',
+    scrollOffset : 0,
+    scrollRatio : 2,
+    scrollingInView : function(){
+      changeMap('https://studio20-2017.github.io/sanctuary/images/westbankmap4.jpg');
+    }
+  });
