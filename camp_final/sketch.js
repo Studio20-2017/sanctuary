e50433febc1a27695896286d3ab061288a1a29c9
Shelley <shelley@shelleys-MacBook-Pro.local>
2017-05-04 13:31:28 -0400

    text size change

--- camp_final/sketch.js
+++ camp_final/sketch.js
@@ -11,22 +11,25 @@ var opp2 = 255;
 var original_image_width = 2000;
 var original_image_height = 1237;
 
+
 function preload(){
   campPop = loadJSON("camp_population_by_month.json");
   state2camp = loadJSON("state_to_camp.json");
-  map = loadImage("us_map-1.jpg");
+  map = loadImage("US MAP light.png");
 }
 
-function setup() {
+function setup() { 
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-
+  
   slider = createSlider (0, 45, 0);
   slider.position(width/2-100, 20);
   textAlign(CENTER);
-  textSize(20);
-  frameRate(1);
-}
+  textSize(40);
+  //frameRate(1); 
+    
+} 
+
 
 // this is a non-p5js function that does some logic for resizing the whole
 // canvas based on the screen size such that it does not crop the graphic.
@@ -52,64 +55,78 @@ function reScale() {
   }
 }
 
-function draw() {
-  if (frameCount < 48){
-    slider.hide();
-  } else {
-    slider.show();
-  }
 
-  reScale();
-   background('#ffffff');
+
+// function Slider(){
+//   slider = createSlider (0, 45, 0);
+// }
+
+function draw() {   
+//   if (frameCount < 48){
+//     slider.hide();    
+//   } else {
+//     slider.show();
+//   }
+ 		reScale();
+   	background('#ffffff');
+    image(map, 0, 0);
+
+  
+   background(220);
     image(map, 0, 0);
 		//console.log(frameCount);
+  
+  //ANIMATION PART:
+  
+  
+//  tl += speed;
+//   if (tl >= 45){
+//   tl = 45;
+//   }
+//   if (frameCount <= 46){
+//   noStroke();
+//   fill(255, 0, 0, 120);
 
- tl += speed;
-  if (tl >= 45){
-  tl = 45;
-  }
-  if (frameCount <= 46){
-  noStroke();
-  fill(255, 0, 0, 120);
-
-  ellipse(150, 340, -campPop[tl]['Tule Lake']/80, -campPop[tl]['Tule Lake']/80);
-  ellipse(430, 530, -campPop[tl]['Topaz_Central Utah']/80, -campPop[tl]['Topaz_Central Utah']/80);
-  ellipse(315, 745, -campPop[tl]['Poston_Colorado River']/80, -campPop[tl]['Poston_Colorado River']/80);
-  ellipse(413, 775, -campPop[tl]['Gila River']/80, -campPop[tl]['Gila River']/80);
-  ellipse(780, 625, -campPop[tl].Granada/80, -campPop[tl].Granada/80);
-  ellipse(570, 300, -campPop[tl]['Heart Mountain']/80, -campPop[tl]['Heart Mountain']/80);
-  ellipse(1180, 840, -campPop[tl].Jerome/80, -campPop[tl].Jerome/80);
-  ellipse(200, 580, -campPop[tl].Manzanar/80, -campPop[tl].Manzanar/80);
-  ellipse(400, 355, -campPop[tl].Minidoka/80, -campPop[tl].Minidoka/80);
-  ellipse(1185, 815, -campPop[tl].Rohwer/80, -campPop[tl].Rohwer/80);
-
-  push();
-  stroke(0);
-  fill(0);
-  text(campPop[tl]['Tule Lake'], 150, 340);
-  text(campPop[tl]['Topaz_Central Utah'], 430, 530);
-  text(campPop[tl]['Poston_Colorado River'], 315, 745);
-  text(campPop[tl]['Gila River'], 413, 775);
-  text(campPop[tl].Granada, 780, 625);
-  text(campPop[tl]['Heart Mountain'], 570, 300);
-  text(campPop[tl].Jerome, 1180, 840);
-  text(campPop[tl].Manzanar, 200, 580);
-  text(campPop[tl].Minidoka, 400, 355);
-  text(campPop[tl].Rohwer, 1185, 815);
-	pop();
-
-  } else {
-
+//   ellipse(150, 340, -campPop[tl]['Tule Lake']/80, -campPop[tl]['Tule Lake']/80);  
+//   ellipse(430, 530, -campPop[tl]['Topaz_Central Utah']/80, -campPop[tl]['Topaz_Central Utah']/80);  
+//   ellipse(315, 745, -campPop[tl]['Poston_Colorado River']/80, -campPop[tl]['Poston_Colorado River']/80);
+//   ellipse(413, 775, -campPop[tl]['Gila River']/80, -campPop[tl]['Gila River']/80);
+//   ellipse(780, 625, -campPop[tl].Granada/80, -campPop[tl].Granada/80);
+//   ellipse(570, 300, -campPop[tl]['Heart Mountain']/80, -campPop[tl]['Heart Mountain']/80);
+//   ellipse(1180, 840, -campPop[tl].Jerome/80, -campPop[tl].Jerome/80);
+//   ellipse(200, 580, -campPop[tl].Manzanar/80, -campPop[tl].Manzanar/80);
+//   ellipse(400, 355, -campPop[tl].Minidoka/80, -campPop[tl].Minidoka/80);
+//   ellipse(1185, 815, -campPop[tl].Rohwer/80, -campPop[tl].Rohwer/80);
+  
+//   push();
+//   stroke(0);
+//   fill(0);
+//   text(campPop[tl]['Tule Lake'], 150, 340);
+//   text(campPop[tl]['Topaz_Central Utah'], 430, 530);
+//   text(campPop[tl]['Poston_Colorado River'], 315, 745);
+//   text(campPop[tl]['Gila River'], 413, 775);
+//   text(campPop[tl].Granada, 780, 625);
+//   text(campPop[tl]['Heart Mountain'], 570, 300);
+//   text(campPop[tl].Jerome, 1180, 840);
+//   text(campPop[tl].Manzanar, 200, 580);
+//   text(campPop[tl].Minidoka, 400, 355);
+//   text(campPop[tl].Rohwer, 1185, 815);
+// 	pop();  
+    
+//   } else {
+  
   fill(0, opp);
   opp+=80;
-  text(campPop[0].Date, width/2-160, 60);
-  text(campPop[45].Date, width/2+150, 60);
-    console.log(slider.value);
+  textSize(38);
+  //text(campPop[0].Date, width/2-200, 60);
+  //text(campPop[45].Date, width/2+200, 60);
+  text(campPop[slider.value()].Date, 900, 95);
+    //console.log(slider.value);
   var x  = slider.value();
   noStroke();
-  fill(255, 0, 0, 120);
+  fill(69,9,32, 150);
   ellipse(150, 340, -campPop[x]['Tule Lake']/80, -campPop[x]['Tule Lake']/80);
-  ellipse(430, 530, -campPop[x]['Topaz_Central Utah']/80, -campPop[x]['Topaz_Central Utah']/80);
+  ellipse(430, 530, -campPop[x]['Topaz_Central Utah']/80, -campPop[x]['Topaz_Central Utah']/80);  
   ellipse(315, 745, -campPop[x]['Poston_Colorado River']/80, -campPop[x]['Poston_Colorado River']/80);
   ellipse(413, 775, -campPop[x]['Gila River']/80, -campPop[x]['Gila River']/80);
   ellipse(780, 625, -campPop[x].Granada/80, -campPop[x].Granada/80);
@@ -118,36 +135,37 @@ function draw() {
   ellipse(200, 580, -campPop[x].Manzanar/80, -campPop[x].Manzanar/80);
   ellipse(400, 355, -campPop[x].Minidoka/80, -campPop[x].Minidoka/80);
   ellipse(1185, 815, -campPop[x].Rohwer/80, -campPop[x].Rohwer/80);
-
+  
     push();
+  textSize(38);
   stroke(0);
-  fill(0);
+  fill(255);
   text(campPop[x]['Tule Lake'], 150, 340);
   text(campPop[x]['Topaz_Central Utah'], 430, 530);
   text(campPop[x]['Poston_Colorado River'], 315, 745);
   text(campPop[x]['Gila River'], 413, 775);
   text(campPop[x].Granada, 780, 625);
   text(campPop[x]['Heart Mountain'], 570, 300);
-  text(campPop[x].Jerome, 1180, 840);
+  text(campPop[x].Jerome, 1180, 850);
   text(campPop[x].Manzanar, 200, 580);
   text(campPop[x].Minidoka, 400, 355);
   text(campPop[x].Rohwer, 1185, 815);
-	pop();
-
-
-  }
+	pop();  
+  
+  
+  //}
   //
-  push();
-  textSize(40);
-  fill(0, opp2);
-  if (frameCount > 45){
-    opp2 = 0;
-  }
-  text(campPop[tl].Date, width/2, 40);
-
-  pop();
-
-
+  // push();
+  // textSize(40);
+  //fill(0, opp2);
+  // if (frameCount > 45){
+  //   opp2 = 0;
+  // }
+  //text(campPop[tl].Date, width/2, 40);
+  
+  //pop();
+  
+  
   push();
   strokeWeight(0.2);
   stroke(255);
@@ -202,7 +220,7 @@ function draw() {
   for (var ca2tl = 500; ca2tl <= state2camp[1]['Tule Lake']/100+500; ca2tl++){
     curve(150, 600, 100, ca2tl, 150, 340, 200, 400);
   }
-
+  
   //hi2topaz
   for (var hi2tp = 500; hi2tp <= state2camp[2].Topaz/20+500; hi2tp++){
     curve(400, 1200, hi2tp, 1100, 430, 530, 350, 600);
@@ -243,40 +261,40 @@ function draw() {
   for (var wa2tl = 200; wa2tl <= state2camp[4]['Tule Lake']/80+200; wa2tl++){
     curve(width/2, 0, wa2tl, 100, 150, 340, width/2, height/2);
   }
-
+  
   pop();
-
-  push();
-  textSize(20);
-  fill(255);
-  text("California", 150, 630);
-  text("Arizona", 450, 700);
-  text("Utah", 450, 500);
-  //text("Oregan", 170, 250);
-  //text("Washington", 220, 100);
-  text("Idaho", 370, 270);
-  text("Wyoming", 650, 400);
-  text("Colorado", 680, 600);
-  text("Arkansas", 1130, 770);
-
+  
+ 
+  textSize(38);
+  // fill(255);
+  // text("California", 170, 655);
+  // text("Arizona", 450, 700);
+  // text("Utah", 450, 490);
+  // //text("Oregan", 170, 250);
+  // //text("Washington", 220, 100);
+  // text("Idaho", 380, 300);
+  // text("Wyoming", 620, 400);
+  // text("Colorado", 680, 600);
+  // text("Arkansas", 1140, 760);
+  
   push();
   strokeWeight(0.5);
-  stroke(0);
-  fill(0);
-  text('Tule Lake', 150, 360);
-  text('Topaz', 430, 550);
-  text('Poston', 315, 765);
-  text('Gila River', 413, 795);
-  text('Granada', 780, 645);
-  text('Heart Mountain', 570, 320);
-  text('Jerome', 1180, 860);
-  text('Manzanar', 200, 600);
-  text('Minidoka', 400, 375);
-  text('Rohwer', 1185, 795);
-	pop();
-
-
+  //stroke(255);
+  fill(255);
+  text('Tule Lake', 150, 380);
+  text('Topaz', 430, 565);
+  text('Poston', 305, 780);
+  text('Gila River', 433, 810);
+  text('Granada', 780, 655);
+  text('Heart Mountain', 580, 260);
+  text('Jerome', 1180, 880);
+  text('Manzanar', 200, 610);
+  text('Minidoka', 400, 395);
+  text('Rohwer', 1295, 815);
+	pop();  
+  
+  
   pop();
-
-
+  
+ 
 }
